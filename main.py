a65e3e0301410c676ea0bdb35546295f3bce6a39
Weliove <gabriu10@hotmail.com>
2021-03-07 07:55:03 -0300

    Init

---
+++ main.py
@@ -0,0 +1,101 @@
+import tkinter as tk
+from tkinter import ttk
+import tkinter.font as font
+from typing import Union
+
+from src.ability.create_ability import CreateAbility
+from src.avatar.create_avatar import CreateAvatar
+from src.home.home import Home
+from src.item.create_item import CreateItem
+from src.proficiency.proficiency_level import ProficiencyLevel
+from src.title.create_title import CreateTitle
+
+
+def check_frame_existence(frame) -> None:
+    if frame is not None and frame.winfo_exists():
+        frame.destroy()
+
+
+class Game(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Game')
+
+        self.resizable(False, False)
+
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+
+        self.frames = dict()
+
+        self.home = None
+        self.create_avatar = None
+        self.create_item = None
+        self.create_ability = None
+        self.create_title = None
+        self.create_proficiency = None
+
+        self.frames = {
+            Home: self.home,
+            CreateAvatar: self.create_avatar,
+            CreateItem: self.create_item,
+            CreateAbility: self.create_ability,
+            CreateTitle: self.create_title
+        }
+
+        self.home = Home(
+            parent=self,
+            create_entity=self.create_entity_frame,
+            proficiencies_level=self.show_proficiencies_level,
+        )
+        self.home.grid(row=0, column=0, sticky='NSEW')
+
+        self.frames[Home] = self.home
+
+        self.show_frame(Home)
+
+    def show_frame(self, container_class) -> None:
+        frame = self.frames[container_class]
+        frame.tkraise()
+
+    def create_entity_frame(self, **kwargs) -> None:
+        container = kwargs['container']
+        container_class = kwargs['container_class']
+        extra_frame = kwargs['extra_frame'] if 'extra_frame' in kwargs else None
+
+        check_frame_existence(container)
+
+        container = container_class(parent=self, home=lambda: self.show_frame(Home), extra_frame=extra_frame)
+        container.grid(row=0, column=0, sticky='NSEW')
+
+        frame = self.set_frames_to_attributes(container_class, container)
+
+        self.frames[container_class] = frame
+        self.show_frame(container_class)
+
+    def set_frames_to_attributes(self, container_class, container):
+        if container_class == CreateAvatar:
+            self.create_avatar = container
+            return self.create_avatar
+        elif container_class == CreateItem:
+            self.create_item = container
+            return self.create_item
+        elif container_class == CreateAbility:
+            self.create_ability = container
+            return self.create_ability
+        elif container_class == CreateTitle:
+            self.create_title = container
+            return self.create_title
+
+    def show_proficiencies_level(self, **kwargs) -> None:
+        ProficiencyLevel(parent=self, **kwargs)
+
+
+root = Game()
+
+style = ttk.Style(root)
+
+font.nametofont('TkDefaultFont').configure(size=12)
+
+root.mainloop()
---
+++ src/title/create_title.py
@@ -0,0 +1,45 @@
+from src.frames.scroll_frame import TemplateScrollFrame
+from src.methods import popup_showinfo, get_text_data, handle_selection_change
+from src.title.title import Title
+from src.title.title_widget import TitleWidget
+
+
+class CreateTitle(TemplateScrollFrame):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.home = kwargs['home']
+
+        self.title_widget = TitleWidget(
+            create_title=self.create_title,
+            widgets=self.template_scroll.widgets,
+            buttons=self.template_scroll.buttons,
+            add_entity_frame=lambda: self.template_scroll.add_entity_frame(TitleWidget)
+        )
+
+        self.set_widgets_conf()
+        self.set_buttons_conf()
+        self.append_to_frames(self.title_widget)
+
+    def create_title(self) -> None:
+        for title_frame in self.template_scroll.frames:
+            character = handle_selection_change(title_frame.character_entry, title_frame.characters)
+            npc = handle_selection_change(title_frame.npc_entry, title_frame.npcs)
+            monster = handle_selection_change(title_frame.monster_entry, title_frame.monsters)
+
+            users = character + npc + monster
+
+            name = title_frame.name.get()
+            requirements = get_text_data(title_frame.requirements_entry)
+            description = get_text_data(title_frame.description_entry)
+
+            title = Title(
+                name=name,
+                requirements=requirements,
+                description=description,
+                users=users
+            )
+
+            create_title = title.create_title()
+
+            self.home() if create_title else popup_showinfo('Something went wrong, please, try again!')
